624daed26ceb591b35915cb1bf95f7f513f2c1d5
ace696134 <acefrankel@yahoo.com>
2026-07-16 10:55:18 -0400

    fewf

--- randomnumbers.lua
+++ randomnumbers.lua
@@ -1,33 +1,39 @@
-local chatBox = peripheral.find("chatBox")
+local cb = peripheral.find("chat_box")
 local speaker = peripheral.find("speaker")
+local monitor = peripheral.find("monitor")
 
-if not chatBox then
+if not cb then
     error("No Chat Box found!")
 end
 if not speaker then
     error("No Speaker found!")
+end
+if not monitor then
+    error("No Monitor found!")
 end
 
 math.randomseed(os.epoch("utc"))
 
 local vars = {}
 
-local function generate(count, min, max)
-    chatBox.sendMessage("Generating random numbers...")
+local function generate(player, count, min, max)
+    cb.sendMessageToPlayer("Generating random numbers...", player)
 
     for i = 0, count do
         vars[i] = math.random(min, max)
-        chatBox.sendMessage(("var%d: %d"):format(i, vars[i]))
+        monitor.write(("var%d: %d"):format(i, vars[i]))
         speaker.playSound("minecraft:block.note_block.harp")
         sleep(1) -- Chat Box has a cooldown
     end
 end
 
 while true do
-    local _, message = os.pullEvent("chat")
+    local _, username, message = os.pullEvent("chat")
 
     if message:lower() == "generate" then
-        generate(5, 1, 9)
+        generate(username, 5, 1, 1000)
         speaker.stop()
     end
+
+
 end
