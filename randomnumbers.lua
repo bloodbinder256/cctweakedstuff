2b682ac5f9d947004ce3a4157ee63f3a60649a1a
bloodbinder256 <bloodbinder256@yahoo.com>
2026-07-16 08:54:44 -0400

    Update and rename test.lua to randomnumbers.lua

---
+++ randomnumbers.lua
@@ -0,0 +1,27 @@
+local cb = peripheral.find("chat_box")
+
+if not cb then
+    error("No Chat Box found!")
+end
+
+math.randomseed(os.epoch("utc"))
+
+local vars = {}
+
+local function generate(player, count, min, max)
+    cb.sendMessageToPlayer("Generating random numbers...", player)
+
+    for i = 0, count do
+        vars[i] = math.random(min, max)
+        cb.sendMessageToPlayer(("var%d: %d"):format(i, vars[i]), player)
+        sleep(1) -- Chat Box has a cooldown
+    end
+end
+
+while true do
+    local _, username, message = os.pullEvent("chat")
+
+    if message:lower() == "generate" then
+        generate(username, 5, 1, 1000)
+    end
+end
