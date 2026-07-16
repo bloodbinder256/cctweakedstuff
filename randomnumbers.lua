fc5b35e76f4984540c608f44dd6c2f295763ac56
ace696134 <acefrankel@yahoo.com>
2026-07-16 09:48:40 -0400

    efwefwe

--- randomnumbers.lua
+++ randomnumbers.lua
@@ -19,9 +19,9 @@ local function generate(player, count, min, max)
         cb.sendMessageToPlayer(("var%d: %d"):format(i, vars[i]), player)
         sleep(1) -- Chat Box has a cooldown
     end
-    speaker.playSound("minecraft:block.note_block.harp")
+    speaker.playSound("minecraft:block.note_block.harp", {volume = 1, pitch = 1})
 end
-
+stop()
 while true do
     local _, username, message = os.pullEvent("chat")
 
