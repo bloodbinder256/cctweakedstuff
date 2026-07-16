694d03b3d448707944b37d5bf26f4bea3bf6c377
ace696134 <acefrankel@yahoo.com>
2026-07-16 15:51:25 -0400

    waf

--- randomnumbers.lua
+++ randomnumbers.lua
@@ -31,7 +31,7 @@ end
 while true do
     local event, arg1, arg2 = os.pullEvent()
 
-    if event == "relay.getInput(front)" then
+    if event == "relay.getInput(front)" == true then
         monitor.clear()
     elseif event == "chat" then
         local username, message = arg1, arg2
