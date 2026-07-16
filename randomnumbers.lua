68041b4ed91a249a31855ee160c8ad921357b6de
ace696134 <acefrankel@yahoo.com>
2026-07-16 10:48:18 -0400

    allplayers

--- randomnumbers.lua
+++ randomnumbers.lua
@@ -11,22 +11,22 @@ math.randomseed(os.epoch("utc"))
 
 local vars = {}
 
-local function generate(player, count, min, max)
-    cb.sendMessageToPlayer("Generating random numbers...", player)
+local function generate(count, min, max)
+    cb.sendMessage("Generating random numbers...")
 
     for i = 0, count do
         vars[i] = math.random(min, max)
-        cb.sendMessageToPlayer(("var%d: %d"):format(i, vars[i]), player)
+        cb.sendMessage(("var%d: %d"):format(i, vars[i]))
         speaker.playSound("minecraft:block.note_block.harp")
         sleep(1) -- Chat Box has a cooldown
     end
 end
 
 while true do
-    local _, username, message = os.pullEvent("chat")
+    local _, message = os.pullEvent("chat")
 
     if message:lower() == "generate" then
-        generate(username, 5, 1, 9)
+        generate(5, 1, 9)
         speaker.stop()
     end
 end
