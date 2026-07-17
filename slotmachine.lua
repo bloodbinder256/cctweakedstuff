23edd77227058898a06afc03e8d286445892b58c
ace696134 <acefrankel@yahoo.com>
2026-07-17 18:12:39 -0400

    wfa

--- slotmachine.lua
+++ slotmachine.lua
@@ -5,3 +5,7 @@ local monitor = peripheral.find("monitor")
 local width, height = monitor.getSize()
 
 print("Monitor size: " .. width .. "x" .. height)
+
+windowLeft = window.create(monitor, 1, 1, width / 3, height)
+windowMiddle = window.create(monitor, 1, 1, width / 3, height)
+windowRight = window.create(monitor, 1, 1, width / 3, height)
