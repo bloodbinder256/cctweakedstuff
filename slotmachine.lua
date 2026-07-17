2e95411b9b68f7eaa789a5d20953420760577201
ace696134 <acefrankel@yahoo.com>
2026-07-17 18:15:43 -0400

    wwa

--- slotmachine.lua
+++ slotmachine.lua
@@ -6,6 +6,12 @@ local width, height = monitor.getSize()
 
 print("Monitor size: " .. width .. "x" .. height)
 
+local windowLeft, windowMiddle, windowRight
+
 windowLeft = window.create(monitor, 1, 1, width / 3, height)
 windowMiddle = window.create(monitor, 1, 1, width / 3, height)
 windowRight = window.create(monitor, 1, 1, width / 3, height)
+
+windowLeft.setBackgroundColor(colors.yellow)
+windowMiddle.setBackgroundColor(colors.blue)
+windowRight.setBackgroundColor(colors.yellow)
