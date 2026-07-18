a92c88fe99aef2fc629df07a4e90c33a710c8347
ace696134 <acefrankel@yahoo.com>
2026-07-17 21:30:13 -0400

    afwasf

--- slotmachine.lua
+++ slotmachine.lua
@@ -39,7 +39,7 @@ end
 local function Title(win, text)
     local w, h = win.getSize()
     local x = math.floor((w - #text) / 2) + 1
-    local y = math.floor(h / 3) + 1
+    local y = math.floor(h / 1) + 1
 
     win.setCursorPos(x, y)
     win.write(text)
